fb55a2cb07fbfc302d43d6331bbea4e94c0da18e
minh <duongdoican@gmail.com>
2022-01-22 23:46:34 +0700

    commit

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,28 +1,72 @@
-let getHomepage = async (req, res) =>
+import pool from '../config/connectDB'
+
+
+let getHomepage = async (req, res) => {
+    pool.query('select * from admin', function (err, result, fields) {
+        const test = result.map(test => { return test })
+        return res.render('home.hbs', { res: test })
+    })
+}
+
+let getLogin = async (req, res) => {
+    res.render('login.hbs')
+}
+
+let goToLogin = async (req, res) => {
+    res.render('login.hbs')
+}
+
+let getHandle = async (req, res) => {
+    let ID = req.body.id;
+    let ten = req.body.ten;
+    
+    pool.query('insert into admin (ID,Ten) values (?,?)', [ID, ten])
+    pool.query('select * from admin', function (err, result, fields) {
+        const test = result.map(test => { return test })
+        return res.redirect('/')
+    })
+}
+
+let getDelete = async (req, res) =>
 {
+    let ID = req.body.id;
+    pool.query('DELETE FROM admin WHERE ID = ?', [ID])
+    pool.query('select * from admin', function (err, result, fields) {
+        const test = result.map(test => { return test })
+        return res.redirect('/')
+    })
+}
+
+let getEdit = async(req,res) =>
+{
+    let ID = req.params.id;
+    pool.query('select * from admin where ID = ?', [ID], function(err,result,fields){
+        const test = result.map(test => { return test })
+        res.render('edit.hbs', {res: test})
+    })
    
-    res.render('home.hbs')
 }
 
-let getLogin = async (req, res) =>
+let getDoEdit = async(req,res) =>
 {
-    res.render('login.hbs')
-}
-
-let goToLogin = async (req,res) =>
-{
-    res.render('login.hbs')
-}
-
-let getHandle = async (req,res) =>
-{
-      
-      return res.render('home.hbs')  
+    let ID = req.body.id;
+    let Ten = req.body.ten;
+    console.log(ID)
+    console.log(Ten)
+    pool.query('update admin set Ten = ? where ID = ?', [Ten, ID] )
+    
+    pool.query('select * from admin', function (err, result, fields) {
+        const test = result.map(test => { return test })
+        return res.redirect('/')
+    })
 }
 
 module.exports = {
     getHomepage,
     getLogin,
     goToLogin,
-    getHandle
+    getHandle,
+    getDelete,
+    getEdit,
+    getDoEdit
 }
--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,9 @@ const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage)
     router.get('/login', homeController.getLogin)
     router.post('/handle', homeController.getHandle)
+    router.post('/delete', homeController.getDelete)
+    router.get('/edit/:id', homeController.getEdit)
+    router.post('/doEdit', homeController.getDoEdit)
     return app.use('/', router)
 }
 
